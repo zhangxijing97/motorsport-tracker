234d2048e811c392f194a2fab7d53e0a98b1cc38
zhangxijing97 <zhangxijing97@gmail.com>
2026-04-23 16:40:37 -0700

    Update  backend/src/main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,17 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.enableCors({
+//     origin: 'http://localhost:3000',
+//   });
+
+//   await app.listen(3001);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,9 +19,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://motorsporttracker.netlify.app',
+    ],
   });
 
-  await app.listen(3001);
+  const port = process.env.PORT ?? 3001;
+  await app.listen(port);
 }
+
 bootstrap();
